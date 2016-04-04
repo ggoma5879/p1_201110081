c5655fb6e1730fc679b31d7cef15e517f3ccd516
ggoma5879 <ggoma5879@gmail.com>
2016-04-04 17:35:45 +0900

    w5
    통합본(triangle, BMI)

---
+++ w5Main.py
@@ -0,0 +1,34 @@
+﻿nMax=20
+munja="*"
+def starTriangle(munja) :
+	for k in range(1, nMax):
+		print " "*(nMax-k) + (munja* k) + ((k-1) * munja)
+
+starTriangle("*")
+
+def computeBMI(weight,height):
+    bmi = float(weight/(height*height))
+    if bmi  <= 18.5:
+        print "low weight"
+    elif bmi < 25.0:
+        print "normal"
+    elif bmi < 30.0:
+        print"high weight"
+    else:
+        print "error"
+
+    print bmi
+
+computeBMI(50.0,1.5)
+
+def lab5():
+    weight = 50.0
+    height = 1.5
+    computeBMI(weight,height)
+    
+def main():
+    lab5()
+    starTriangle(munja)
+    
+if __name__=="__main__":
+    main()
