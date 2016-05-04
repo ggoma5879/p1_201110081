842c7881cc0d7acc43f3c5f12b978d9bb4e84bfd
ggoma5879 <ggoma5879@gmail.com>
2016-05-04 10:11:09 +0900

    w9a
    w9a

---
+++ w9Main.py
@@ -0,0 +1,30 @@
+﻿
+import matplotlib
+import matplotlib.pyplot as plt
+
+def charCount(word):
+    word=word
+    mydict=dict()
+    for c in word: 
+        if c not in mydict:
+            mydict[c]=1
+        else:
+            mydict[c]=mydict[c]+1
+    return mydict
+
+
+def drawPlt(mydict):
+    plt.bar(range(len(mydict)), mydict.values(),align='center')
+    plt.xticks(range(len(mydict)),list(mydict.keys()))
+    plt.show()
+
+def lab9():
+    mydict=dict()
+    mydict=charCount("my name is sung mo kim and i love jay park")
+    drawPlt(mydict)
+
+def main():
+    lab9()
+
+if __name__ == "__main__":
+    main()
---
+++ w9_1Main.py
@@ -0,0 +1,33 @@
+﻿import matplotlib
+import matplotlib.pyplot as plt
+
+def drawPlt(mydict):
+    plt.bar(range(len(mydict)), mydict.values(),align='center')
+    plt.xticks(range(len(mydict)),list(mydict.keys()))
+    plt.show()
+
+def countAlphaDigitLetters(word):
+    word=word
+    mydict=dict()
+    count=0
+    countDigit=0
+    for c in word: 
+        if c.isalpha():
+            count = count + 1
+            mydict['alpha']=count
+        elif c.isdigit():
+            countDigit = countDigit + 1
+            mydict['digit']=countDigit
+    return mydict
+
+def lab9_1():
+
+    mydict=countAlphaDigitLetters("SANGMYUNG UNIVERSITY, 20, HONGIMUN 2-GIL, JONGNO-GU, SEOUL, REPUBLIC OF KOREA ")
+    drawPlt(mydict)
+
+def main():
+    lab9_1()
+
+
+if __name__ == "__main__":
+    main()
