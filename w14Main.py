26936a509dfddd3697bd643b55684c5767aa26b0
ggoma5879 <ggoma5879@gmail.com>
2016-06-01 15:46:18 +0900

    객체지향
    객체지향

---
+++ w14Main.py
@@ -0,0 +1,42 @@
+﻿import turtle
+
+class SquareTurtle(turtle.Turtle):
+    def drawSquare(self):
+        for i in range(0,4):
+            self.fd(50)
+            self.right(90)
+    def drawSquare(self,size):
+        for i in range(0,4):
+            self.fd(size)
+            self.right(90)
+square1 = SquareTurtle()
+square1.drawSquare(20)
+
+class DogSound(object):
+    def __init__(self, name):
+        self.name = name
+    def getName(self):
+        print "my dog name is" , self.name
+    def talk(self):
+        print self.name,"mung mung"
+
+class ShihTzuDog(object):
+    def __init__(self, name):
+        self.name = name
+    def talk(self):
+        print self.name,"si si"
+        
+class maltese(object):
+    def __init__(self, name):
+        self.name = name
+    def talk(self):
+        print self.name,"mal mal"
+
+si = ShihTzuDog("sizu")
+si.talk()
+
+mal = maltese("malte")
+mal.talk()
+
+pu = DogSound("puppy")
+pu.talk()
